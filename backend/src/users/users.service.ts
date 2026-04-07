176fc37f2f1f16e8f0ee728407140d6f5a4e46cf
suhailphp <suhailphp@gmail.com>
2026-04-07 18:37:49 +0400

    fix(core): resolve type, lint, and config errors

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
@@ -12,13 +12,17 @@ export class UsersService {
   ) {}
 
   async create(user: Partial<User>): Promise<User> {
+    if (!user.password) {
+      throw new BadRequestException('Password is required');
+    }
     const salt = await bcrypt.genSalt();
     user.password = await bcrypt.hash(user.password, salt);
     const newUser = this.usersRepository.create(user);
     return this.usersRepository.save(newUser);
   }
 
-  async findOne(username: string): Promise<User | undefined> {
+  async findOne(username: string): Promise<User | null> {
     return this.usersRepository.findOne({ where: { username } });
   }
 }
+
