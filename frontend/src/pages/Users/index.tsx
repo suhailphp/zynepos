77b5c231fc714259eff2d4b1a137529a636b9c4f
suhailphp <suhailphp@gmail.com>
2026-04-08 13:57:46 +0400

    feat(frontend): initial custom layout and theme setup

---
+++ frontend/src/pages/Users/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout, Typography } from 'antd';
+
+const { Title } = Typography;
+
+const UsersPage: React.FC = () => {
+  return (
+    <>
+      <Title>User Management</Title>
+      <p>This is where the user list, user creation, and user editing forms will go.</p>
+    </>
+  );
+};
+
+export default UsersPage;
